330c190109c3d14c0a911fd83b7663e992563668
Felix Siegmeier <fe.siegmeier@gmail.com>
2023-05-08 21:33:31 +0200

    navbar implemented

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -8,8 +8,8 @@ export default function Navbar (){
   return (
     <nav className="bg-discord flex items-center justify-between flex-wrap p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link href="/">
-          <a className="text-2xl font-semibold">Logo</a>
+        <Link href="/" className="text-2xl font-semibold">
+          Logo
         </Link>
       </div>
       <div className="block lg:hidden">
@@ -25,10 +25,12 @@ export default function Navbar (){
         }`}
       >
         <div className="text-sm lg:flex-grow">
-          <Link href="/aerzte">
-            <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+          <Link
+            href="/aerzte"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+            
               Ärzte
-            </a>
+            
           </Link>
           <div className="relative mt-4 lg:inline-block lg:mt-0">
             <button
@@ -42,29 +44,33 @@ export default function Navbar (){
                 isDropdownOpen ? "block" : "hidden"
               } absolute bg-gray-800 text-white py-2 mt-2 rounded-lg`}
             >
-              <Link href="/dienstplan/plan1">
-                <a className="block px-4 py-2 hover:bg-gray-700">Plan 1</a>
+              <Link href="/dienstplan/plan1" className="block px-4 py-2 hover:bg-gray-700">
+                Plan 1
               </Link>
-              <Link href="/dienstplan/plan2">
-            <a className="block px-4 py-2 hover:bg-gray-700">Plan 2</a>
+              <Link href="/dienstplan/plan2" className="block px-4 py-2 hover:bg-gray-700">
+            Plan 2
           </Link>
-          <Link href="/dienstplan/plan3">
-            <a className="block px-4 py-2 hover:bg-gray-700">Plan 3</a>
+          <Link href="/dienstplan/plan3" className="block px-4 py-2 hover:bg-gray-700">
+            Plan 3
           </Link>
         </div>
       </div>
-      <Link href="/configuration">
-        <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
-          Configuration
-        </a>
+      <Link
+        href="/configuration"
+        className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+        
+          Einstellungen
+        
       </Link>
-      <Link href="/logout">
-        <a className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white">
+      <Link
+        href="/logout"
+        className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white">
+        
           Logout
-        </a>
+        
       </Link>
     </div>
   </div>
 </nav>
-  )
+  );
             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        discord: "#7289da",
+      },
+    },
   },
   plugins: [],
 }
